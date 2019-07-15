c6bcceab928f004501be17b828fcb0731fb68d12
La Fountain <github email address>
2019-07-14 22:24:49 -0400

    Add react router and components, all working

--- client/auxilary_app/src/component/SideNav.js
+++ client/auxilary_app/src/component/SideNav.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Drawer, makeStyles } from "@material-ui/core";
 import List from "./SimpleList";
+import { Link } from "react-router-dom";
 
 const sideLinks = [
-  ["Home", "home"],
-  ["Exchange", "store"],
-  ["Wallet", "account_balance_wallet"],
+  [<Link to="/home">Home</Link>, "home"],
+  [<Link to="/exchange">Exchange</Link>, "store"],
+  [<Link to="/wallet">Wallet</Link>, "account_balance_wallet"],
   ["Withdraw", "card_giftcard"],
   ["Deposit", "payment"],
-  ["Trading", "euro_symbol"],
+  [<Link to="/trading">Trading</Link>, "euro_symbol"],
   ["News", "web"],
   ["Watchlist", "format_list_numbered"],
   ["Portfolio", "work"]
--- client/auxilary_app/src/index.js
+++ client/auxilary_app/src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./container/App";
+import { BrowserRouter as Router } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
