81f3b5f61608d6af95aeb147ccd5594921973aeb
La Fountain <github email address>
2019-07-15 22:07:39 -0400

    Finish trading ui, just change color and text styling

---
+++ client/auxilary_app/src/component/Textfield.js
@@ -0,0 +1,17 @@
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+
+const TradingTextfield = props => {
+  return (
+    <React.Fragment>
+      <TextField
+        // className={classes.textField}
+        // value={values.name}
+        // onChange={handleChange("name")}
+        margin="normal"
+      />
+    </React.Fragment>
+  );
+};
+
+export default TradingTextfield;
---
+++ client/auxilary_app/src/component/TradingGrid.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Divider from "@material-ui/core/Divider";
+import TradingButton from "./TradingButton";
+import TradingTable from "./TradingTable";
+
+const useStyles = makeStyles(theme => ({
+  grid: {
+    marginTop: 200,
+    marginLeft: 200,
+    width: "80rem"
+  },
+  subGrid: {
+    marginTop: 60,
+    marginLeft: 200,
+    width: "80rem"
+  },
+  innerContainer: {
+    marginTop: 10
+  },
+  paper: {
+    height: "27rem"
+  },
+  moneyContainer: {
+    // marginTop: 60,
+    // padding: "1.3rem"
+  },
+  moneyContainer2: {
+    padding: "1.3rem"
+  }
+}));
+
+const TradingGrid = () => {
+  const classes = useStyles();
+  return (
+    <Grid container spacing={2} className={classes.grid}>
+      <Grid item md={6}>
+        <Paper>
+          <h2>Buy</h2>
+          <Grid container className={classes.moneyContainer}>
+            <TradingTable price={"Bid"} />
+          </Grid>
+          <TradingButton color={"#2e7d32"} name={"buy"} />
+        </Paper>
+      </Grid>
+      <Grid item md={6}>
+        <Paper>
+          <h2>Sell</h2>
+          <Grid container className={classes.moneyContainer}>
+            <TradingTable price={"Ask"} />
+          </Grid>
+          <TradingButton color={"#ff3d00"} name={"sell"} />
+        </Paper>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default TradingGrid;
