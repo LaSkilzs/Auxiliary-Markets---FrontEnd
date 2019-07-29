f5989e09a55ac34fedf90559fc4db2eea8977456
NaimJones16 <naim.jones@yahoo.com>
2019-07-29 15:54:38 -0400

    Update AdminForm.js

--- auxilary_markets/client/src/component/AdminForm.js
+++ auxilary_markets/client/src/component/AdminForm.js
@@ -2,22 +2,45 @@ import React from "react";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import green from "@material-ui/core/colors/green";
+import ContractAid from "truffle-contract";
+import Web3 from "web3";
+import ExContract from "../contracts/build/Exchange.json";
 
 class AdminForm extends React.Component {
   constructor() {
     super();
     this.state = {
       name: "",
-      address: ""
+      address: 0
     };
+    const provider = new Web3.providers.HttpProvider("http://127.0.0.1:8545");
+    this.web3 = new Web3(provider);
   }
+
 
   handleChange = e => this.setState({ [e.target.name]: e.target.value });
   handleSubmit = () => {
     const token = this.state;
-    console.log(token);
+
+
     // call exchange function to add token
+    console.log(token);
   };
+
+  async Store() {
+    const ExchangeAddress = '0xfad009BF255B8c5473c6A961b36FF91124DD2748';
+    const Exchange = ContractAid(ExContract);
+    Exchange.setProvider(this.web3.givenProvider);
+    let check = false;
+    Exchange.at(ExchangeAddress).then(function(Instance) {
+    }).then(function(result) {
+      if(Exchange.hasToken() === check)
+        alert("Basuraaa!!! Token not yet Created!");
+      else
+      check = true;
+        return Exchange.addToken(this.state.symbol, this.state.address)
+    });
+  }
   render() {
     const primary = green[600];
     return (
