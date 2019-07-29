f642ef4efa98f05b9635f125a77aa6c5a46f33b7
NaimJones16 <naim.jones@yahoo.com>
2019-07-29 15:52:59 -0400

    Update CreateAsset.js

--- auxilary_markets/client/src/component/CreateAsset.js
+++ auxilary_markets/client/src/component/CreateAsset.js
@@ -1,3 +1,4 @@
+// import java.util.*;
 import React from "react";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
@@ -5,33 +6,74 @@ import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import { Typography } from "@material-ui/core";
 import green from "@material-ui/core/colors/green";
+import ContractAid from "truffle-contract";
+import Web3 from "web3";
+import ExContract from "../contracts/build/Exchange.json";
 
-class CreateAsset extends React.Component {
+
+
+class CreateAsset extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
       symbol: "",
       name: "",
+      address: "",
       total_supply: 0,
       market_cap: 0,
       circulating: 0,
-      highest: 0,
-      price: 0,
+      highest: 0.00,
+      price: 0.00,
       units: 0
     };
+    // const ExchangeContract = require('../contracts/build/Exchange.json');
+    // var Web3 = require('web3');
+    // var web3 = new Web3('http://127.0.0.1:8545');
+    const provider = new Web3.providers.HttpProvider("http://127.0.0.1:8545");
+    this.web3 = new Web3(provider);
+
   }
+  // async waitandStore(){
+  //   const timeout = async ms => new Promise(res => setTimeout(res, ms));
+  //   let next = false;
+  //   while (next === false) await timeout(100); // pause script but avoid browser to freeze ;)
+  //   next = false; // reset var
+  //   return next;
+  //
+  // }
 
   handleSubmit = () => {
     const token = this.state;
     console.log(token);
     // Contract Call to Store Data
+    // var web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545'));
+    const ExchangeAddress = '0xfad009BF255B8c5473c6A961b36FF91124DD2748';
+    const Exchange = ContractAid(ExContract);
+    Exchange.setProvider(this.web3.givenProvider);
+    // Exchange = Exchange.at(ExchangeAddress);
+    let check = false;
+    // let symbol = newCoin.hasToken(this.state.symbol);
+    let addy = this.state.address
+    // waitandStore().then();
+    Exchange.at(ExchangeAddress).then(function(Instance) {
+      // let newCoin = Instance;
+    }).then(function(result) {
+      if(Exchange.hasToken() === check)
+        alert("Basuraaa!!! Already Created ");
+      else
+      check = true;
+        return Exchange.createToken(this.state.address, this.state.symbol)
+    });
+    console.log("This is the data for " + this.state.name + " coin Address: " + addy + " Symbol: " + this.state.symbol);
   };
+
 
   handleChange = e => this.setState({ [e.target.name]: e.target.value });
 
   render() {
     const primary = green[600];
     return (
+
       <Grid container spacing={3} style={{ marginLeft: 100 }}>
         <Paper style={{ marginTop: 100, height: "55rem" }}>
           <Typography
@@ -146,11 +188,24 @@ class CreateAsset extends React.Component {
                 shrink: true
               }}
             />
+            <TextField
+              label="Address"
+              style={{ margin: 8, width: "35rem" }}
+              placeholder="e.g.0x89CA3efb8b466EA592F0Adce9E1BE0213B9c64A2"
+              margin="normal"
+              variant="outlined"
+              name={"address"}
+              value={this.state.address}
+              onChange={e => this.handleChange(e)}
+              InputLabelProps={{
+                shrink: true
+              }}
+            />
             <Grid item lg={6} style={{ marginLeft: 280, margin: "auto" }}>
               <Button
                 variant="contained"
                 size="large"
-                onClick={() => this.handleSubmit()}
+                onClick= {e => this.handleSubmit(e)}
                 style={{
                   width: "35rem",
                   marginTop: 30,
